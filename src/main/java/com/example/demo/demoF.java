6f54dedad540efb596ea8196f98779f6096d9436
Yogesh Ganesh Trivedi <yg3vedi1988@gmail.com>
2024-09-22 18:14:43 +0530

    demoF add

---
+++ src/main/java/com/example/demo/demoF.java
@@ -0,0 +1,11 @@
+package com.example.demo;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class demoF {
+    @GetMapping("/demoF")
+    public String getData() {return "DEMOC DDDDDDDDDDDDDDDDDDDDDD I’m Yogesh Trivedi, excited to join as a DevOps learner and collaborate with you all. I have a solid background in software development with a focus on Java, and I’m now transitioning into the DevOps space to enhance my skills in automation, CI/CD, cloud infrastructure, and containerization. I’m particularly interested in working with tools like Jenkins, Docker, Kubernetes, and cloud platforms like AWS. My goal is to improve development efficiency by bridging the gap between development and operations. I’m eager to learn from your experiences, contribute to ongoing projects, and grow my knowledge in setting up pipelines, managing infrastructure, and ensuring smooth releases. I look forward to adding value to the team while embracing the DevOps culture of collaboration and continuous improvement.\n" +
+            "Thank you, and I’m excited for the journey ahead!\n"; }
+}
