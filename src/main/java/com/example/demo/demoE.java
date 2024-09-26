8f351afed6ef28abc9f6c8fd1fcaded54626cbce
Yogesh Ganesh Trivedi <yg3vedi1988@gmail.com>
2024-09-27 01:17:02 +0530

    classE added

---
+++ src/main/java/com/example/demo/demoE.java
@@ -0,0 +1,10 @@
+package com.example.demo;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class demoE {
+    @GetMapping("/demoE")
+    public String getData() {return "Hi Guys this is my 1st project in DEVOPS Class E devloped - Yogesh Trivedi" ; }
+}
